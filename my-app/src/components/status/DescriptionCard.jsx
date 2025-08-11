207e32e2ff60641c966dbab2e7b6b9fe0d5383a0
sohailaomara <sohailaoomara@gmail.com>
2025-08-11 16:29:11 +0300

    modified description card

--- my-app/src/components/status/DescriptionCard.jsx
+++ my-app/src/components/status/DescriptionCard.jsx
@@ -1,6 +1,6 @@
 export default function DescriptionCard({ name, description }) {
   return (
-    <div className="bg-gray-200 border border-gray-100 p-4 rounded w-48">
+    <div className="bg-white shadow-md shadow-gray-300 border border-gray-100 p-4 rounded w-48">
       <h3 className="font-bold mb-2">{name}</h3>
       <p className="text-sm">
         {description && description.trim() !== ""
