72df4d516a0a1dff698a1f80ef6059bbd935a940
sohailaomara <sohailaoomara@gmail.com>
2025-09-23 15:05:50 +0300

    chatbot icon without chatbot page

--- my-app/src/App.jsx
+++ my-app/src/App.jsx
@@ -6,7 +6,6 @@ import PrivateRoute from "./components/auth/PrivateRoute";
 import HomePage from "./pages/HomePage";
 import Vlanpage from "./pages/Vlanpage";
 import PortOps from "./pages/PortOps";
-import ChatbotPage from "./pages/ChatbotPage";
 
 function App() {
   return (
@@ -49,7 +48,6 @@ function App() {
       />
       <Route path="/vlan" element={<Vlanpage />} />
       <Route path="/portops" element={<PortOps />} />
-      <Route path="/chatbot" element={<ChatbotPage />} />
     </Routes>
   );
 }
